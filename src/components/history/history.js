fd7008d64cb69e8550976fd79b92d038060e08ed
Shubham Mishra <shubhamishra583@gmail.com>
2023-01-11 18:19:42 +0530

    fixed membership bug

--- src/components/history/history.js
+++ src/components/history/history.js
@@ -46,7 +46,7 @@ function History() {
             <div className="birth-text-holder">
               <div className="marker-container">
                 <div className="marker">
-                  <div className="marker-date"></div>
+                  <div className="marker-date">2008</div>
                 </div>
 
                 <div className="history-title-holder">
@@ -71,41 +71,6 @@ function History() {
               src="/assets/images/history-demo-image.png"
               alt=""
             />
-          </Col>
-        </Row>
-        <Row className="birth-section-container">
-          <Col xs={24} lg={12} className="birth-image-container">
-            <img
-              className="birth-img-holder"
-              src="/assets/images/history-demo-image.png"
-              alt=""
-            />
-          </Col>
-
-          <Col xs={24} lg={12}>
-            <div className="birth-text-holder">
-              <div className="marker-container">
-                <div className="marker">
-                  <div className="marker-date"></div>
-                </div>
-
-                <div className="history-title-holder">
-                Our Transformation
-                </div>
-                <div className="red-highlight"></div>
-
-                <div className="birth-text">
-                <p>5 key pillars as areas to focus our efforts for the transformation required with a proper structure and governance, which should drive the success of these pillars.</p>
-                {/* <ul> */}
-                  <p><span className="transformPoints">Sports</span>  - We want to amplify the values of the current sports activities and enhance the positive impact it has on our members.</p>
-                  <p><span className="transformPoints">Community Engagement</span> - We want to reach out and deepen relationships with the wider Indian communities, forge new partnerships with other reputable Indian organizations and strengthen existing collaboration with community leaders and institutions.</p>
-                  <p><span className="transformPoints">Membership Benefits</span> - Our members are the most important asset of our Association. We will design a comprehensive membership benefits scheme to give all members access to a privileged experience. Improve and implement new programs and assistance services.</p>
-                  <p><span className="transformPoints">Revenue Growth</span> – We need to ensure that SIA explores its full potential to drive revenue generation and growth opportunities through existing facilities and services.</p>
-                  <p><span className="transformPoints">Better Future</span> – We need to drive this transformational roadmap to take our Association forward to the next level and to newer heights and for us to build a better and brighter future for our Singapore Indian Association.</p>
-                {/* </ul> */}
-                </div>
-              </div>
-            </div>
           </Col>
         </Row>
       </div>
